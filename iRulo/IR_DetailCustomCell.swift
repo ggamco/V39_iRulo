4a0bc54bb09208639356c68d0ec6efd0024e2119
cice <cice@aula8PovMacN08.local>
2017-04-07 22:05:52 +0200

    primeros ajustes

---
+++ iRulo/IR_DetailCustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  IR_DetailCustomCell.swift
+//  iRulo
+//
+//  Created by cice on 7/4/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+import UIKit
+
+class IR_DetailCustomCell: UICollectionViewCell {
+    
+}
